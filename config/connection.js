503bc947563cd72b9a98f8d2a6c2315696e48907
JD Tadlock <jt7903@gmail.com>
2023-04-11 15:01:51 -0400

    ready for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const connection = new Sequelize(
-  process.env.DATABASE,
-  process.env.USERNAME,
-  process.env.PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+
+const connection = new Sequelize('mysql://r5l8gr7tcuttgpm4:jtqm62baplfidd7l@l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/aj91s4swdmnfthol');
 
 module.exports = connection;
