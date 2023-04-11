4e5d85df6562816c8d5e9e7a8bf93b400874da41
JD Tadlock <jt7903@gmail.com>
2023-04-11 14:38:04 -0400

    sequelize intro app complete

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
+const db = require('./config/connection');
+const api_routes = require('./routes/api_routes');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
 
-app.listen(PORT, () => console.log('Server listening on port %s', PORT));
+app.use('/api', api_routes);
+
+db.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Server listening on port %s', PORT));
+});
